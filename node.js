d716fef2860c72733d06f0405224b56bf29fa0ea
Lev Kochetkov <108297418+KLL00-1@users.noreply.github.com>
2022-11-09 22:44:19 +0300

    Update node.js

--- node.js
+++ node.js
@@ -1,49 +1,44 @@
 async function getInfo() {
-  let responce = await fetch("https://sheetdb.io/api/v1/3rj1uo0drlg53");
+  let responce = await fetch(
+    "https://raw.githubusercontent.com/KLL00-1/marketPlace/master/info.json"
+  );
   let responceInfo = await responce.json();
   let i;
-  for (i = 0; i < responceInfo.length; i++) {
+  // for (i = 0; i < responceInfo.length; i++)
+  for (i in responceInfo) {
     const nftCards = document.querySelector(".NFT_cards");
     nftCards.innerHTML += `
-      <div class="NFT_card ${responceInfo[i].petAndRarity}">
-          <div>
-            <img class="card_img1" src="${responceInfo[i].faceImage}" alt="" />
+        <div class="NFT_card ${responceInfo[i].petAndRarity}">
+            <div>
+              <img class="card_img1" src="${responceInfo[i].faceImage}" alt="" />
+            </div>
+            <div class="like">
+              <button class="btn1" type="submit">
+                <img src="cards img/heart.png" alt="" />
+              </button>
+              <h2 class="counter">${responceInfo[i].innerLike}</h2>
+            </div>
+            <div class="pet_name">
+              <h2>${responceInfo[i].nameAndNumber}</h2>
+              <span class="epic ${responceInfo[i].rarity}">${responceInfo[i].rarity}</span>
+            </div>
+            <div class="lvl_mint"><span>Lvl: 0</span><span>Mint: 0/3</span></div>
+            <div class="price_and_buy">
+              <img src="cards img/Vector.svg" alt="" />
+              <h2>${responceInfo[i].price}</h2>
+              <a href="">BUY IT NOW</a>
+            </div>
           </div>
-          <div class="like">
-            <button class="btn1">
-              <img src="cards img/heart.png" alt="" />
-            </button>
-            <h2 class="counter">${responceInfo[i].innerLike}</h2>
-          </div>
-          <div class="pet_name">
-            <h2>${responceInfo[i].nameAndNumber}</h2>
-            <span class="epic ${responceInfo[i].rarity}">${responceInfo[i].rarity}</span>
-          </div>
-          <div class="lvl_mint"><span>Lvl: 0</span><span>Mint: 0/3</span></div>
-          <div class="price_and_buy">
-            <img src="cards img/Vector.svg" alt="" />
-            <h2>${responceInfo[i].price}</h2>
-            <a href="">BUY IT NOW</a>
-          </div>
-        </div>
-      `;
+        `;
   }
 }
-
 getInfo();
-// function getNft() {
-//   let responce = fetch("https://sheetdb.io/api/v1/3rj1uo0drlg53").then(
-//     (responce) => responce.json()
-//   );
 
-//   console.log(responce[0]);
-// }
-// getNft();
 /*
-dog_common
-dog_uncommon
-dog_epic
-cat_common
-cat_uncommon
-cat_epic
-*/
+  dog_common
+  dog_uncommon
+  dog_epic
+  cat_common
+  cat_uncommon
+  cat_epic
+  */
